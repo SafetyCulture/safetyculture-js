ddb16da4edd2fb23a49bd0413a56dd06bce7fd9a
Nick Matenaar <nick@prismatik.com.au>
2015-11-04 17:44:28 +1100

    Add api wrapper

---
+++ src/api.js
@@ -0,0 +1,58 @@
+import rp from 'request-promise';
+import _ from 'lodash';
+
+export const BASE_URL = 'https://api.safetyculture.io';
+
+export function generateToken(username, password) {
+  return rp({
+    method: 'POST',
+    uri: BASE_URL + '/auth',
+    body: {
+      username,
+      password,
+      grant_type: 'password'
+    }
+  })
+  .then(body => body.access_token)
+  .catch(() => { throw new Error('Invalid username or password'); });
+}
+
+/**
+* Create a SafetyCulture Api client
+* @param {object} opts The following options
+* @param {string} token Valid token for authorization requests
+* @returns {object} api Api client
+*/
+export default function Api({ token }) {
+  const defaultOptions = {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    json: true
+  };
+
+  return {
+    /**
+    * Posts body to endpoint on SafetyCulture
+    * @param {string} endpoint The endpoint to post to on SafetyCulture
+    * @param {object} body The body to post
+    * @returns {Promise} Resolves to body of response,
+    *                    Rejects with an error from API.
+    */
+    post: (endpoint, options) => rp(_.assign({}, defaultOptions, {
+      method: 'POST',
+      uri: BASE_URL + endpoint
+    }, options)),
+
+    /**
+    * Gets from endpoint on SafetyCulture
+    * @param {string} endpoint The endpoint to get from on SafetyCulture
+    * @returns {Promise} Resolves to body of response,
+    *                    Rejects with an error from API.
+    */
+    get: (endpoint, options) => rp(_.assign({}, defaultOptions, {
+      uri: BASE_URL + endpoint
+    }, options))
+
+  };
+}
