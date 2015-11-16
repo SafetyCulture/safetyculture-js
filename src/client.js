25aef3dfada84073755ff5bd1d326d0ee9d9ffbe
Nick Matenaar <nick@prismatik.com.au>
2015-11-17 10:55:32 +1100

    Add doc to client

--- src/client.js
+++ src/client.js
@@ -1,6 +1,12 @@
 import Api from './api';
 import Audits from './audits';
 
+/**
+* Generates Client methods based off passed in token
+* @param {object} options
+* @param {string} options.token SafetyCulture access token
+* @returns {object} SafetyCulture API Client
+*/
 export default function Client({ token }) {
   const api = Api({ token });
 
