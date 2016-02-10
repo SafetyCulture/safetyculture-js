9f163c2c9eeff2e66db5d1e09e93298b974b2061
John D'Agostino <john.dagostino@safetyculture.io>
2016-02-10 18:40:06 +1100

    rename module to safetyculture rather than javascript-sdk

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import Api from './api';
 import Audits from './audits';
 
-const DEFAULT_LOGGER = {info: () => {},
+const DEFAULT_LOGGER = {info: () => { console.log(arguments); },
                         error: () => {}};
 /**
 * Generates Client methods based off passed in token
@@ -10,6 +10,8 @@ const DEFAULT_LOGGER = {info: () => {},
 * @returns {object} SafetyCulture API Client
 */
 export default function Client({ token, logger = DEFAULT_LOGGER}) {
+  logger.info(`SafetyCulture Client ${logger}`);
+
   const api = Api({ token });
 
   return {
---
+++ src/safetyculture.js
@@ -0,0 +1,7 @@
+import Client from './client';
+
+const SafetyCulture = {
+  Client
+};
+
+export default SafetyCulture;
