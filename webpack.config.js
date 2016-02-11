b47c02a062b50110d696ea0fd52ba491b3aa290d
Brett Porter <brett.porter@safetyculture.io>
2016-02-11 17:14:06 +1100

    clean up after gulp removal

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-    entry  : './lib/safetyculture.js',
+    entry  : './lib/index.js',
     output : {
         path     : 'dist/',
         filename : 'safetyculture.dist.js'
     },
     module : {
-        loaders: [ {
-                test   : /.js$/,
-                loader : 'babel-loader'
-            }
+        loaders: [ 
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            { test: /\.json$/, loader: "json"}
         ]
+    },
+    externals: {
+        fs: '{}',
+        net: '{}',
+        tls: '{}',
+        "cls-bluebird": {}
     }
 };
